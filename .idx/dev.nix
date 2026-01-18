c17794f53c09db7f5c4eff34b90674bcb2a9ad62
bayupriatno007 <bayupriatno007@gmail.com>
2026-01-18 08:59:34 +0000

    Changes before Firebase Studio auto-run

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,44 +1,15 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
-  # Which nixpkgs channel to use.
-  channel = "stable-23.11"; # or "unstable"
-  # Use https://search.nixos.org/packages to find packages
+  # Let Go applications use the network.
+  previews = [
+    {
+      command = ["go", "run", "velora/cmd/server/main.go"];
+      manager = "web";
+      port = 8080;
+    }
+  ];
+
+  # Needed for running Go.
   packages = [
     pkgs.go
-    pkgs.nodejs_20
-    pkgs.nodePackages.nodemon
-    pkgs.docker
   ];
-  # Sets environment variables in the workspace
-  env = {};
-  idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [
-      "golang.go"
-      "google.gemini-cli-vscode-ide-companion"
-    ];
-    workspace = {
-      onCreate = {
-        # Open editors for the following files by default, if they exist:
-        default.openFiles = ["server.go"];
-      };
-    };
-    # Enable previews and customize configuration
-    previews = {
-      enable = true;
-      previews = {
-        web = {
-          command = [
-            "nodemon"
-            "--signal" "SIGHUP"
-            "-w" "."
-            "-e" "go,html"
-            "-x" "go run server.go -addr localhost:$PORT"
-          ];
-          manager = "web";
-        };
-      };
-    };
-  };
 }
